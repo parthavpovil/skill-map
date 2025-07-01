c658730849c1451d126ef15cf5bc305b7b919688
parthavpovil <parthavpovil@gmail.com>
2025-07-01 21:15:18 +0530

    completed first api

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import(
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+func main(){
+
+	fmt.Println("Skill Map")
+
+	router := gin.Default()
+  	router.GET("/", func(c *gin.Context) {
+    c.JSON(200, gin.H{
+      "message": "skill-map-home-page",
+    })
+  })
+  router.Run()
+}
